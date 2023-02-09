a24a423005d6e3578e15967d561919df72836391
NguyenVanHieuPH14434 <nguyenvanhieugl2001@gmail.com>
2023-02-09 10:42:01 +0700

    update 9/2

--- app/Constant/Constanst.php
+++ app/Constant/Constanst.php
@@ -8,7 +8,7 @@ class Constanst {
     const PARENT = 0;
     const LIMIT_PERPAG = 10;
     const UNCONFIMRER = 1;
-    const BASE_URL = 'http://localhost:8000/' || 'http://hieu.fresher.ameladev.click/';
+    const BASE_URL = 'http://hieu.fresher.ameladev.click/';
     const DASHBOARD_URL = self::BASE_URL . 'admin/dashboard'; 
     const PERMISSION_URL = self::BASE_URL . 'admin/permissions'; 
     const CREATE_PERMISSION_URL = self::BASE_URL . 'admin/permissions/create'; 
