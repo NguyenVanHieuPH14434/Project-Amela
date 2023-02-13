b9c7e1a9a899df99bdaa9fa3fbc8a5554081d28a
NguyenVanHieuPH14434 <nguyenvanhieugl2001@gmail.com>
2023-02-13 16:50:40 +0700

    update 13/02

--- resources/views/testa.blade.php
+++
@@ -1,7 +0,0 @@
-@foreach ($product->attributeProduct as $i)
-    {{-- @foreach ($attr as $it) --}}
-        @if($i->pivot->product_id == 21 && $i->pivot->color_id == 14 && $i->pivot->size_id == 11)
-            {{ $i->pivot->price }}
-        @endcan
-    {{-- @endforeach --}}
-@endforeach
--- resources/views/testaa.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach ($product->attributeProduct as $key => $it)
-    {{-- @if($it->attr_name == $it->attr_name) --}}
-    @if($product->sizeProduct[$key]->pivot->id == $it->pivot->id)
-         {{ $it->pivot->stock }}
-    @endif
-@endforeach
-
-{{-- {{ $product->attributeProduct->pivot->id }} --}}
